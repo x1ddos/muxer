39cd7f4cd0bf33cd0df10648bd6cf871f0e59f98
alex <alex@cloudware.it>
2012-11-07 14:56:44 +0100

    Actually, that didn't work out for http://go.pkgdoc.org/code.google.com/p/go-muxer. Let's try again.

--- muxer.go
+++ muxer.go
@@ -1,29 +1,30 @@
-// Simple muxer without regexp.
-// Usage example:
-// 
-//	package myapp
-//
-//	import (
-//		"net/http"
-//		muxer "code.google.com/p/go-muxer"
-//	)
-//
-//	func handler1(w http.ResponseWriter, r *http.Request, v url.Values) {
-//		// v will be populated with params from URL path, if any.
-//		// v.Get("id")
-//		// v.Get("action")
-//	}
-//
-//	func init() {
-//		m := muxer.NewMux("/api", nil)
-//		m.Add("GET", "users/{id}", handler1).As("profile")
-//		m.Add("GET", "products", handler2)
-//		m.Add("PUT", "products/{id}/do", handler3)
-//		m.Add("POST", "{domain}/{action}/{id}", handler4).As("whatever")
-//	}
-//	
-// See muxer_test.go for more.
+/*
+Simple muxer without regexp.
+Usage example:
 
+	package myapp
+
+	import (
+		"net/http"
+		muxer "code.google.com/p/go-muxer"
+	)
+
+	func handler1(w http.ResponseWriter, r *http.Request, v url.Values) {
+		// v will be populated with params from URL path, if any.
+		// v.Get("id")
+		// v.Get("action")
+	}
+
+	func init() {
+		m := muxer.NewMux("/api", nil)
+		m.Add("GET", "users/{id}", handler1).As("profile")
+		m.Add("GET", "products", handler2)
+		m.Add("PUT", "products/{id}/do", handler3)
+		m.Add("POST", "{domain}/{action}/{id}", handler4).As("whatever")
+	}
+
+See muxer_test.go for more.
+*/
 package muxer
 
 import (
